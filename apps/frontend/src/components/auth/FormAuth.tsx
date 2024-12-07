16fe8233f8da22c6b04593619ff80e5e57b36a7b
Marllon Ramos <marllonsteam@gmail.com>
2024-12-07 08:59:53 -0800

    Adicionar botão ao formulário de autenticação

--- apps/frontend/src/components/auth/FormAuth.tsx
+++ apps/frontend/src/components/auth/FormAuth.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import CaixaFormularios from "../shared/CaixaFormularios";
@@ -6,34 +6,46 @@ import CampoEmail from "../shared/CampoEmail";
 import CampoSenha from "../shared/CampoSenha";
 import Logo from "../shared/Logo";
 import GoogleAuth from "./GoogleAuth";
-
+import Botao from "../shared/Botao";
 
 export default function FormAuth() {
-   return (
-    <div className="flex flex-col justify-center items-center h-screen">        
-         <CaixaFormularios>
-            <div className="flex flex-col items-center">         
-                 <Logo/>
-                 <span className="text-2xl font-bold">Entre com sua conta</span>
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <CaixaFormularios>
+        <div className="flex flex-col items-center">
+          <Logo />
+          <span className="text-2xl font-bold">Entre com sua conta</span>
+        </div>
+        <CampoEmail />
+        <CampoSenha />
+        <Botao cor="verde">Login</Botao>
+        <div className="flex items-center justify-center">
+          <div className="border-t border-zinc-500 flex-grow"></div>
+          <span className="px-2 text-zinc-600">OU</span>
+          <div className="border-t border-zinc-500 flex-grow"></div>
+        </div>
+        <GoogleAuth />
+        <div className="flex justify-center">
+          <div className="flex flex-col items-center">
+            <div>
+              Ainda não possui conta?{" "}
+              <span className="text-[#22C55E] text-lg font-normal">
+                Cadastre-se
+              </span>{" "}
+              <Link
+                href="/cadastro"
+                className="underline text-[#22C55E] text-lg font-normal"
+              >
+                aqui
+              </Link>
             </div>
-            <CampoEmail/>
-            <CampoSenha/>
-            <div className="flex items-center justify-center">
-                <div className="border-t border-zinc-500 flex-grow"></div>
-                <span className="px-2 text-zinc-600">OU</span>
-                <div className="border-t border-zinc-500 flex-grow"></div>
-            </div>
-            <GoogleAuth/>           
-            <div className="flex justify-center">                                                       
-                    <div className="flex flex-col items-center">
-                        <div>
-                            Ainda não possui conta? {' '}
-                            <span className="text-[#22C55E] text-lg font-normal">Cadastre-se</span> <Link href="/cadastro" className="underline text-[#22C55E] text-lg font-normal">aqui</Link>
-                         </div>   
-                        <span className="text-xs font-extralight text-zinc-500">ou faça Login pelo Google clicando no G acima</span>
-                    </div>                                                            
-            </div>  
-         </CaixaFormularios>            
+
+            <span className="text-xs font-extralight text-zinc-500">
+              ou faça Login pelo Google clicando no G acima
+            </span>
+          </div>
+        </div>
+      </CaixaFormularios>
     </div>
-   )
+  );
 }
--- apps/frontend/src/components/shared/CampoEmail.tsx
+++ apps/frontend/src/components/shared/CampoEmail.tsx
@@ -1,29 +1,28 @@
 import { IconMail } from "@tabler/icons-react";
 import NomeCampo from "./NomeCampo";
 
-
-export interface CampoEmailProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    onChangeText?: (s: string) => void
+export interface CampoEmailProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  onChangeText?: (s: string) => void;
 }
 
-export default function CampoEmail (props: CampoEmailProps) {   
-   return ( 
-        <div>
-            <NomeCampo nome="Email"/>
-        <div className="flex input items-center outline-1">          
-            <input                             
-                type ="email" 
-                value={props.value}
-                onChange={(e) => {
-                    props.onChange?.(e)
-                    props.onChangeText?.(e.target.value);
-                }}
-                placeholder={props.placeholder}                       
-                   className="flex-1 outline-none bg-transparent"
-                
-            />
-            <IconMail size={20} className="text-zinc-400"/>
-        </div>
-        </div>
-    )
+export default function CampoEmail(props: CampoEmailProps) {
+  return (
+    <div>
+      <NomeCampo nome="Email" />
+      <div className="flex input items-center outline-1">
+        <input
+          type="email"
+          value={props.value}
+          onChange={(e) => {
+            props.onChange?.(e);
+            props.onChangeText?.(e.target.value);
+          }}
+          placeholder={props.placeholder}
+          className="flex-1 outline-none bg-transparent"
+        />
+        <IconMail size={20} className="text-zinc-400" />
+      </div>
+    </div>
+  );
 }
