898680aad0a21d5f416a7cbeaff8bfe9c576698e
Marllon Ramos <marllonsteam@gmail.com>
2024-12-21 00:07:55 -0800

    +Feat/FormMudarSenha +Fix/FormRecuperarSenha +Delete/PaginaRecuperarSenha
    - Criação do FormMudarSenha
    - Criação do CampoNovaSenha
    - Criação do CampoConfirmarNovaSenha
    - Alteração do FormRecuperarSenha(Adicionei uma div externa responsavel por centralizar o form assim como o componente FormAuth faz. Entendo que essa seria uma função da pagina e não do componente, porém segui conforme o desenvolvimento anterior do componente FormAuth.)
    - Exclusão da pagina criada anterior mente de recuperação de senha, pois pertence ja a pagina "entrar" e não terá uma página dedicada.
    -Propriedade modo do "CampoEmail" e do "CampoSenha" alterada para opcional ja que não estava sendo utilizada.

--- apps/frontend/src/components/shared/CampoEmail.tsx
+++ apps/frontend/src/components/shared/CampoEmail.tsx
@@ -4,7 +4,7 @@ import NomeCampo from "./NomeCampo";
 export interface CampoEmailProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   onChangeText?: (s: string) => void;
-  modo : string;
+  modo?: string;
 }
 
 export default function CampoEmail(props: CampoEmailProps) {
