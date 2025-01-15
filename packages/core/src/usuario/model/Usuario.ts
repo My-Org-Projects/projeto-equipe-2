f6f90678d21c641513cbd73538c4bec1a387ceaf
fernandes <storeshopad@gmail.com>
2025-01-15 01:54:58 -0300

    Rafael (Inclusão do Objeto de valor Telefone- campo telefone no Prisma - Atributo Telefone no Usuario 3 )

--- packages/core/src/usuario/model/Usuario.ts
+++ packages/core/src/usuario/model/Usuario.ts
@@ -1,22 +1,26 @@
 import Email from "../../shared/Email";
 import Entidade, { EntidadeProps } from "../../shared/Entidade";
 import NomePessoa from "../../shared/NomePessoa";
+import Telefone from "../../shared/Telefone";
 
 export interface UsuarioProps extends EntidadeProps {   
     nome?: string
     email: string
     senha?: string
+    telefone?: string
 }
 
 export default class Usuario extends Entidade<Usuario, UsuarioProps > {
     readonly nome: NomePessoa
     readonly email: Email
     readonly senha: String
+    readonly telefone: Telefone
 
     constructor (props: UsuarioProps) {
         super(props) 
         this.nome = new NomePessoa(props.nome)
         this.email = new Email(props.email)
+        this.telefone = new Telefone(props.telefone)
         this.senha = props.senha
     }  
     
