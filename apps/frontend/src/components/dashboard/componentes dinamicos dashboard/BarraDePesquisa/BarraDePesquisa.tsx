bf1b214c053a93714a62476d1da74fe059edf00e
Marllon Ramos <marllonsteam@gmail.com>
2025-01-30 11:00:01 -0300

    +Integracao da ListaDeUsuarios com o endpoint de retorno de usuarios

--- apps/frontend/src/components/dashboard/componentes dinamicos dashboard/BarraDePesquisa/BarraDePesquisa.tsx
+++ apps/frontend/src/components/dashboard/componentes dinamicos dashboard/BarraDePesquisa/BarraDePesquisa.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import InputPesquisa from "./InputPesquisa";
 
-const BarraDePesquisa = () => {
+type BarraDePesquisaProps = {
+  filtro: string;
+  setFiltro: (valor: string) => void;
+};
+
+const BarraDePesquisa = ({ filtro, setFiltro }: BarraDePesquisaProps) => {
   return (
     <div className="bg-[#09090B] w-[708px] h-[57px] flex gap-7 justify-center items-center p-2 border border-white/20 rounded-xl">
       <span className="text-lg text-[#F5F5F5] ml-4">Pesquisar Usuários</span>
-      <InputPesquisa />
+      <InputPesquisa filtro={filtro} setFiltro={setFiltro} />
     </div>
   );
 };
